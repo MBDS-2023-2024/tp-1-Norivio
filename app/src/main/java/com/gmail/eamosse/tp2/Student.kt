14f39e588b3c051bc37b12865dc582a3b91ef55a
Ivionel Norgaisse <inorgaisse@solutions.ht>
2024-07-23 09:11:49 -0400

    Créer une classe Etudiant (name, code, sexe (M, F), adresse, age)

--- app/src/main/java/com/gmail/eamosse/tp2/Student.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/Student.kt
@@ -1,6 +1,6 @@
 package com.gmail.eamosse.tp2
 
 //TODO : Complete the class
-data class Student {
+data class Student (val name: String, val code: String, val sexe: String, val adresse: String, val age: Int){
 
 }
--- app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
+++ app/src/test/java/com/gmail/eamosse/tp2/LocalStudentManagerTest.kt
@@ -55,11 +55,11 @@ class LocalStudentManagerTest {
     @Test
     fun boy_isCorrect() {
         val boys = listOf(
-            Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20),
-            Student(name = "John4", code = "A4", sexe = "M", address = "Lilles", age = 42),
-            Student(name = "John5", code = "A5", sexe = "M", address = "Lilles", age = 34),
-            Student(name = "John7", code = "A7", sexe = "M", address = "Lilles", age = 28),
-            Student(name = "John8", code = "A8", sexe = "M", address = "Lilles", age = 16),
+            Student(name = "John1", code = "A1", sexe = "M", adresse = "Lilles", age = 20),
+            Student(name = "John4", code = "A4", sexe = "M", adresse = "Lilles", age = 42),
+            Student(name = "John5", code = "A5", sexe = "M", adresse = "Lilles", age = 34),
+            Student(name = "John7", code = "A7", sexe = "M", adresse = "Lilles", age = 28),
+            Student(name = "John8", code = "A8", sexe = "M", adresse = "Lilles", age = 16),
         )
         val result = manager.boysOrGirls("M")
         assertArrayEquals("", boys.toTypedArray(), result.toTypedArray())
@@ -68,11 +68,11 @@ class LocalStudentManagerTest {
     @Test
     fun girl_isCorrect() {
         val girls = listOf(
-            Student(name = "John2", code = "A2", sexe = "F", address = "Lilles", age = 30),
-            Student(name = "John3", code = "A3", sexe = "F", address = "Lilles", age = 41),
-            Student(name = "John6", code = "A6", sexe = "F", address = "Lilles", age = 12),
-            Student(name = "John9", code = "A9", sexe = "F", address = "Lilles", age = 10),
-            Student(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56),
+            Student(name = "John2", code = "A2", sexe = "F", adresse = "Lilles", age = 30),
+            Student(name = "John3", code = "A3", sexe = "F", adresse = "Lilles", age = 41),
+            Student(name = "John6", code = "A6", sexe = "F", adresse = "Lilles", age = 12),
+            Student(name = "John9", code = "A9", sexe = "F", adresse = "Lilles", age = 10),
+            Student(name = "John10", code = "A10", sexe = "F", adresse = "Lilles", age = 56),
         )
         val result = manager.boysOrGirls("F")
         assertArrayEquals("", girls.toTypedArray(), result.toTypedArray())
@@ -120,9 +120,9 @@ class LocalStudentManagerTest {
     @Test
     fun reversed_isCorrect() {
         val last =
-            Student(name = "John10", code = "A10", sexe = "F", address = "Lilles", age = 56)
+            Student(name = "John10", code = "A10", sexe = "F", adresse = "Lilles", age = 56)
         val first =
-            Student(name = "John1", code = "A1", sexe = "M", address = "Lilles", age = 20)
+            Student(name = "John1", code = "A1", sexe = "M", adresse = "Lilles", age = 20)
         // When
         val result = manager.reverse()
         // Then last should become first and vice versa
