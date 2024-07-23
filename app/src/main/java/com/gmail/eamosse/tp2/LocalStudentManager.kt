ef3287c30864d839ccb1ba4d941dc43af5b42870
Ivionel Norgaisse <inorgaisse@solutions.ht>
2024-07-23 10:50:30 -0400

    Créer une liste contenant uniquement le nom des étudiants

--- app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
+++ app/src/main/java/com/gmail/eamosse/tp2/LocalStudentManager.kt
@@ -40,7 +40,7 @@ class LocalStudentManager : StudentManager {
     }
 
     override fun onlyNames(): List<String> {
-        TODO("Not yet implemented")
+        return students.map { it.name }
     }
 
     override fun deleteBySex(sex: String) {
